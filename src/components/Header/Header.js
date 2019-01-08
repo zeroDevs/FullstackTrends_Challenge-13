a111f850551b2287c855eb5d4fb9ec2ad51543c5
Akin Aguda <lordtitan7@gmail.com>
2019-01-08 14:09:03 +0100

    added navbar resixe transition and margin between buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <section >
             <nav id="header" className="navbar navbar-expand-md navbar-light fixed-top">
-            <Link to="top" smooth={true} duration={800} offset={-131}><img src={logo} alt="logo" height="50" /></Link>
+            <Link to="top" smooth={true} duration={800} offset={-131}><img src={logo} alt="logo" height="50" className="logo-active" /></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
