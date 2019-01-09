9eab0d3e486bcdb5bec3beafb41fb9ec66cafa75
David Sima <davidsima@windowslive.com>
2019-01-09 06:54:06 +0200

    Adding missing bracket

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Navigation.css';
 
-const Navigation = ({ onNavClick, currentCategoryIndex }) => (
+const Navigation = ({ onNavClick, currentCategoryIndex }) => {
   const isActive = (categoryIndex) => {
         return `button-primary${categoryIndex===currentCategoryIndex?' active':''}`;
     }
