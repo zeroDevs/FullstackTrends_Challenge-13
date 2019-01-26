2d54cf0f3cd8ed9dafd29f20cb808c4493cbf8ae
luxumbra <hello@crafted.im>
2019-01-26 14:44:56 +0000

    Moving Button css to component css file. #163

--- src/components/Navigation/Button.js
+++ src/components/Navigation/Button.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import './Button.css';
+// import './Button.css';
 
 const Button = props => (
-  <button
-    type={props.btnType}
-    btnText={props.btnText}
-    className={props.className}
-    onClick={props.onClick}
-  >
+  <button type={props.btnType} className={props.className} onClick={props.onClick}>
     <i className={`fas ${props.iconClass}`} />
     <span className="d-none d-sm-block">{props.btnText}</span>
   </button>
