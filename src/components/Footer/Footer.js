9e82f68778e0bc7be880e720cf2b8d9217df2674
Mike Perry Y Attara <31483629+mikeattara@users.noreply.github.com>
2019-01-28 16:50:38 -0500

    remove duplicate contributor profiles
    I removed duplicate profiles for Matt and Ankur

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -42,16 +42,17 @@ const Footer = ({ contrib }) => (
               <Modal>
                 <li className="grow">Zero To Mastery</li>
               </Modal>
-              <a href="https://github.com/anantankur" target="_blank" rel="noopener noreferrer">
-                <li className="grow">Ankur Anant</li>
-              </a>
-              <a href="https://github.com/Dhaval1403" target="_blank" rel="noopener noreferrer">
-                <li className="grow">Dhaval Mehta</li>
-              </a>
-              <a href="https://github.com/MattCSmith" target="_blank" rel="noopener noreferrer">
-                <li className="grow">Matt Smith</li>
-              </a>
-              <Contrib contributors={contrib} />
+                <li className="grow">
+                  <a href="https://github.com/Dhaval1403" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                  >
+                    Dhaval Mehta
+                  </a>
+                </li>
+                </li className="grow">
+                  <Contrib contributors={contrib} />
+                </li>
             </ul>
           </div>
         </div>
